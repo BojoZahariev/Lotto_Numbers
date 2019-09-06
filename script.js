f92b06808aa263cce926911a9464de4b539fc879
Bojo <bozhidar_zahariev@hotmail.com>
2019-09-06 11:08:21 +0100

    working

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var num1 = document.getElementById('num1');
-var num2 = document.getElementById('num2');
-
 function RNG(seed) {
 	// LCG using GCC's constants
 	this.m = 0x80000000; // 2**31;
@@ -28,69 +25,59 @@ RNG.prototype.choice = function(array) {
 	return array[this.nextRange(0, array.length)];
 };
 
-//Seed
-var firstName = 'Bojo';
-let input1 = document.getElementById('birthDate');
-var dateBirth = Date.parse(input1.value);
-var currentDate = new Date();
-
-//get the number for the day
-var today = currentDate.getDate() + currentDate.getMonth() + 1 + currentDate.getFullYear();
-
-var letterToNumber = (str) => {
-	str = str.toLowerCase();
-	var sum = 0;
-	for (let i = 0; i < str.length; i++) {
-		sum += str.charCodeAt(i);
-	}
-	return sum;
-};
-
-//lucky numbers change every day
-var rng = new RNG(letterToNumber(firstName) + dateBirth + today);
-
-var myArray = [];
-
-let randomNumber = rng.nextRange(1, 51);
-/*
-myArray.push(randomNumber);
-myArray.forEach(function(element) {
-	let counter;
-	if (myArray.indexOf(randomNumber) === -1 && counter < 5) {
-		myArray.push(randomNumber);
-		counter++;
-	}
-});
-*/
-
-let i = 0;
-while (i < 10) {
-	if (myArray.indexOf(randomNumber) === -1) {
-		myArray.push(randomNumber);
-		randomNumber = rng.nextRange(1, 51);
-		i++;
-	} else {
-		console.log('nuc' + randomNumber);
-		randomNumber = rng.nextRange(1, 51);
-	}
-}
-
-//testing that
-var numbersUnique = Array.from(new Set(myArray));
-
 const generateNumbers = () => {
+	//Seed
+	let firstName = 'Bojo';
+	let input1 = document.getElementById('birthDate');
+	let dateBirth = Date.parse(input1.value);
+	let currentDate = new Date();
+
+	//get the number for the day
+	var today = currentDate.getDate() + currentDate.getMonth() + 1 + currentDate.getFullYear();
+
+	var letterToNumber = (str) => {
+		str = str.toLowerCase();
+		var sum = 0;
+		for (let i = 0; i < str.length; i++) {
+			sum += str.charCodeAt(i);
+		}
+		return sum;
+	};
+
+	//lucky numbers change every day
+	var rngSeed = new RNG(letterToNumber(firstName) + dateBirth + today);
+
+	var myArray = [];
+
+	let randomNumber = rngSeed.nextRange(1, 51);
+
+	let i = 0;
+	while (i < 6) {
+		if (myArray.indexOf(randomNumber) === -1) {
+			myArray.push(randomNumber);
+			randomNumber = rngSeed.nextRange(1, 51);
+			i++;
+		} else {
+			console.log('nuc' + randomNumber);
+			randomNumber = rngSeed.nextRange(1, 51);
+		}
+	}
+
 	for (let i = 0; i < myArray.length; i++) {
 		var item = document.createElement('li');
 		item.classList.add('item');
-		const container = document.querySelector('#container');
+		const container = document.getElementById('container');
 		container.appendChild(item);
 		item.textContent = myArray[i];
-		//item.textContent = rng.nextRange(1, 51);
-		//item.textContent = numbersUnique[i];
 	}
+
+	return container;
 };
 
-let generateButton = document.getElementById('generateButton');
-generateButton.addEventListener('click', () => {
+let displayButton = document.getElementById('displayButton');
+displayButton.addEventListener('click', () => {
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
 	generateNumbers();
 });
